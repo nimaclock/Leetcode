1d40606405c6847d34ed4acf5ae1415a509f1b4e
Mingjie Wu <mingjiewu@MingjiedeMacBook-Air.local>
2019-12-26 22:37:24 -0800

    338

--- DP/338-counting-bits.java
+++ DP/338-counting-bits.java
@@ -1,7 +1,22 @@
 // https://leetcode.com/problems/counting-bits/
-    public int[] countBits(int num) {
+
+// optimal 
+   public int[] countBits(int num) {
         int[] res = new int[num+1];
         for (int i = 0; i <= num; i++)
             res[i] = res[i >> 1] + (i & 1);
+        return res;
+    }
+
+// personal
+    public int[] countBits(int num) {
+        int[] res = new int[num+1];
+        res[0] = 0;
+        if (num >=1) res[1] = 1;
+        int rem = 2;
+        for (int i = 2; i <= num; i++) {
+            if (i > rem*2) rem *= 2;
+            res[i] = res[i%rem] + 1;
+        }
         return res;
     }
