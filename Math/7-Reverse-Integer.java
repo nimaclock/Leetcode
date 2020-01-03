633a4aea891597630eeaeed434f2d5690e2e6a70
Mingjie Wu <mingjie@amazon.com>
2020-01-02 20:30:44 -0800

    7

---
+++ Math/7-Reverse-Integer.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/reverse-integer/
+class Solution {
+    public int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res*10 + x % 10;
+            x /= 10;
+        }
+        return (int) res == res ? (int) res : 0;
+    }
+}
